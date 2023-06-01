3f676e5c34139efde1494a3a3f8bd4421438b6d7
Badr Saud <134721278+badr-saud@users.noreply.github.com>
2023-06-01 17:58:33 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const img = document.querySelector(".img");
+const container = document.querySelector(".container");
+
+function phones(phone) {
+  img.src = phone;
+}
+function colors(color) {
+  container.style.backgroundColor = color;
+}
